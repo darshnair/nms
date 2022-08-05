8e95917b0101799cd01dbd0fe70a132ca73398ff
Darsh Nair <darsh.nair8@gmail.com>
2022-08-05 18:27:42 +0530

    Application StartUp Bootstrap

---
+++ src/test/java/com/nms/NmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
