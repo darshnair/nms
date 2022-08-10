518d1ce4b379adbd7ac81c6032a2bcb2589cbe1d
Darsh Nair <darsh.nair8@gmail.com>
2022-08-11 00:00:51 +0530

    Refactor

--- nms-auth/src/main/java/com/nms/auth/entity/Movie.java
+++ nms-auth/src/main/java/com/nms/auth/entity/Movie.java
@@ -1,48 +1,16 @@
 package com.nms.auth.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
 
 	private Long movieId;
 	private String movie_name;
 	private String movie_genre;
-
-	public Movie() {
-	}
-
-	public Movie(Long movieId, String movie_name, String movie_genre) {
-		this.movieId = movieId;
-		this.movie_name = movie_name;
-		this.movie_genre = movie_genre;
-	}
-
-	public Movie(Long movieId, String movie_name) {
-		super();
-		this.movieId = movieId;
-		this.movie_name = movie_name;
-	}
-
-	public Long getMovieId() {
-		return movieId;
-	}
-
-	public void setMovieId(Long movieId) {
-		this.movieId = movieId;
-	}
-
-	public String getMovie_name() {
-		return movie_name;
-	}
-
-	public void setMovie_name(String movie_name) {
-		this.movie_name = movie_name;
-	}
-
-	public String getMovie_genre() {
-		return movie_genre;
-	}
-
-	public void setMovie_genre(String movie_genre) {
-		this.movie_genre = movie_genre;
-	}
 
 }
