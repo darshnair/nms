bea5707f3f6a72474394eb315b1fab91dd7cd228
Darsh Nair <darsh.nair8@gmail.com>
2022-08-10 22:36:33 +0530

    Refactor

--- nms-auth/src/main/java/com/nms/auth/controller/HomeController.java
+++ nms-auth/src/main/java/com/nms/auth/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/login")
-public class Home {
+public class HomeController {
 
 	@Autowired
 	private RestTemplate restTemplate;
